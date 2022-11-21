71425ffb4cac001b92cadbf408ef11d98bbaa13a
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-21 08:55:17 -0800

    services page added

---
+++ src/components/Services.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  width: 50%;
+`;
+
+const Image = styled.div`
+  height: 100%;
+`;
+const Right = styled.div`
+  width: 50%;
+`;
+
+const Services = () => {
+  return <div>Services</div>;
+};
+
+export default Services;
